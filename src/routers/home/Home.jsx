ca58f2c189edf1d5e82c5f9f446b613ac46a99ae
ahrorbek69 <axausa69@gmail.com>
2022-06-21 21:37:55 +0500

    alibaba

---
+++ src/routers/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RequestMultiple from '../../components/request-multiple/RequestMultiple'
+
+const Home = () => {
+  return (
+    <div>
+      
+        <RequestMultiple />
+
+    </div>
+  )
+}
+
+export default Home
